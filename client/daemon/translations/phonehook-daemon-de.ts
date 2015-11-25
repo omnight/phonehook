ecb6ef62f9f2996df628ad0d53634d4d9d8515d8
Johan Ekman <omnight@gmail.com>
2015-11-25 16:35:14 +0100

    * 0.6.0-2

---
+++ client/daemon/translations/phonehook-daemon-de.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>gui</name>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="38"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="39"/>
+        <source>Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="40"/>
+        <source>Country</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="42"/>
+        <source>Operator</source>
+        <extracomment>network operator</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="44"/>
+        <source>Properties</source>
+        <extracomment>owner properties</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="46"/>
+        <source>SSN</source>
+        <extracomment>social security number/ personal number</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="47"/>
+        <source>Marital Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="48"/>
+        <source>Vehicles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="49"/>
+        <source>Organization Number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="50"/>
+        <source>Company Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="51"/>
+        <source>Remarks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="52"/>
+        <source>Owner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="53"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="54"/>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="55"/>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="56"/>
+        <source>Profession</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="57"/>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="58"/>
+        <source>Comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="59"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="60"/>
+        <source>Age</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="205"/>
+        <source>No results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="411"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="432"/>
+        <source>Block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="476"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ client/daemon/translations/phonehook-daemon-sv.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>gui</name>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="38"/>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="39"/>
+        <source>Address</source>
+        <translation>Adress</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="40"/>
+        <source>Country</source>
+        <translation>Land</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="42"/>
+        <source>Operator</source>
+        <extracomment>network operator</extracomment>
+        <translation>Operatör</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="44"/>
+        <source>Properties</source>
+        <extracomment>owner properties</extracomment>
+        <translation>Fastighetsinnehav</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="46"/>
+        <source>SSN</source>
+        <extracomment>social security number/ personal number</extracomment>
+        <translation>Personnummer</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="47"/>
+        <source>Marital Status</source>
+        <translation>Civilstånd</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="48"/>
+        <source>Vehicles</source>
+        <translation>Fordonsinnehav</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="49"/>
+        <source>Organization Number</source>
+        <translation>Organisationsnummer</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="50"/>
+        <source>Company Information</source>
+        <translation>Företagsinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="51"/>
+        <source>Remarks</source>
+        <translation>Anmärkningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="52"/>
+        <source>Owner</source>
+        <translation>Ägare</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="53"/>
+        <source>Error</source>
+        <translation>Fel</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="54"/>
+        <source>Email</source>
+        <translation>E-post</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="55"/>
+        <source>Website</source>
+        <translation>Webbsida</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="56"/>
+        <source>Profession</source>
+        <translation>Yrke</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="57"/>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="58"/>
+        <source>Comment</source>
+        <translation>Kommentar</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="59"/>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="60"/>
+        <source>Age</source>
+        <translation>Ålder</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="205"/>
+        <source>No results</source>
+        <translation>Inga resultat</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="411"/>
+        <source>Back</source>
+        <translation>Tillbaka</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="432"/>
+        <source>Block</source>
+        <translation>Blockera</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="476"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+</context>
+</TS>
