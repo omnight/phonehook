a60305a1492c10b65b48f7dcb1c273c2758c6ce8
Carmen F. B <carmenfdezb@yahoo.es>
2016-01-06 17:47:01 +0100

    Add daemon file translation to Spanish

---
+++ client/daemon/translations/phonehook-daemon-es.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>gui</name>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="38"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="39"/>
+        <source>Address</source>
+        <translation>Dirección</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="40"/>
+        <source>Country</source>
+        <translation>País</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="42"/>
+        <source>Operator</source>
+        <extracomment>network operator</extracomment>
+        <translation>Operador</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="44"/>
+        <source>Properties</source>
+        <extracomment>owner properties</extracomment>
+        <translation>Propiedades</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="46"/>
+        <source>SSN</source>
+        <extracomment>social security number/ personal number</extracomment>
+        <translation>NSS</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="47"/>
+        <source>Marital Status</source>
+        <translation>Estado civil</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="48"/>
+        <source>Vehicles</source>
+        <translation>Vehículos</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="49"/>
+        <source>Organization Number</source>
+        <translation>Número de empresa</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="50"/>
+        <source>Company Information</source>
+        <translation>Información de la compañía</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="51"/>
+        <source>Remarks</source>
+        <translation>Comentarios</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="52"/>
+        <source>Owner</source>
+        <translation>Propietario</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="53"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="54"/>
+        <source>Email</source>
+        <translation>Correo eletrónico</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="55"/>
+        <source>Website</source>
+        <translation>Sitio Web</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="56"/>
+        <source>Profession</source>
+        <translation>Profesión</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="57"/>
+        <source>Category</source>
+        <translation>Categoría</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="58"/>
+        <source>Comment</source>
+        <translation>Comentario</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="59"/>
+        <source>Status</source>
+        <translation>Estado</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="60"/>
+        <source>Age</source>
+        <translation>Edad</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="205"/>
+        <source>No results</source>
+        <translation>No hay resultados</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="411"/>
+        <source>Back</source>
+        <translation>Volver</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="432"/>
+        <source>Block</source>
+        <translation>Bloquear</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="476"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+</context>
+</TS>
