836d87cbc4b1a5f8b58d063ad8057d97226f14e8
Carmen F. B <carmenfdezb@yahoo.es>
2016-01-06 15:29:17 +0100

    Added translations to Spanish

---
+++ client/gui/translations/phonehook-es.ts
@@ -0,0 +1,436 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Phonehook</source>
+        <translation>Phonehook</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Phonehook</source>
+        <translation>Phonehook</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Data Sources</source>
+        <translation>Fuentes de datos</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>Blocks</source>
+        <translation>Bloqueos</translation>
+    </message>
+    <message>
+        <source>Call Log</source>
+        <translation>Registro de llamadas</translation>
+    </message>
+    <message>
+        <source>Daemon</source>
+        <translation>Servicio</translation>
+    </message>
+    <message>
+        <source>Running</source>
+        <translation>En funcionamiento</translation>
+    </message>
+    <message>
+        <source>Not Running</source>
+        <translation>No está en funcionamiento</translation>
+    </message>
+    <message>
+        <source>Start Daemon</source>
+        <translation>Iniciar servicio</translation>
+    </message>
+    <message>
+        <source>Installed Sources</source>
+        <translation>Fuentes instaladas</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <extracomment>No installed sources</extracomment>
+        <translation>No hay fuentes instaladas</translation>
+    </message>
+</context>
+<context>
+    <name>PageAppSettings</name>
+    <message>
+        <source>App Settings</source>
+        <translation>Ajustes de la aplicación</translation>
+    </message>
+    <message>
+        <source>Basic</source>
+        <translation>Basico</translation>
+    </message>
+    <message>
+        <source>Show only for unknown contacts</source>
+        <translation>Mostrar sólo para contactos desconocidos</translation>
+    </message>
+    <message>
+        <source>Old popup design</source>
+        <translation>Diseño antiguo de ventana de diálogo</translation>
+    </message>
+    <message>
+        <source>Enable while roaming</source>
+        <translation>Habilitar en itinerancia</translation>
+    </message>
+    <message>
+        <source>Popup Timeout</source>
+        <translation>Duración de la ventana de diálogo</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Desactivado</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Avanzado</translation>
+    </message>
+    <message>
+        <source>Source Test Mode</source>
+        <translation>Fuentes en modo de prueba</translation>
+    </message>
+    <message>
+        <source>Kill dialer when blocking call</source>
+        <translation>Suprimir el teclado numérico al bloquear llamada</translation>
+    </message>
+    <message>
+        <source>Restart Daemon</source>
+        <translation>Reiniciar servicio</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockAddManual</name>
+    <message>
+        <source>Add Block</source>
+        <translation>Añadir a bloqueo</translation>
+    </message>
+    <message>
+        <source>Add a number to be blocked. Number may be either local or international variety. Use * for wildcard matching.</source>
+        <translation>Añade el número de teléfono que quieres bloquear. El numero puede ser local o internacional. Utiliza el * como comodín.</translation>
+    </message>
+    <message>
+        <source>Hidden number</source>
+        <extracomment>toggle switch for hidden number</extracomment>
+        <translatorcomment>Activa esta opción para números ocultos</translatorcomment>
+        <translation>Número oculto</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <extracomment>placeholder for name input</extracomment>
+        <translatorcomment>lugar para introducir nombre</translatorcomment>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <source>Phone Number</source>
+        <extracomment>placeholder for number input</extracomment>
+        <translatorcomment>Lugar para introducir número de teléfono</translatorcomment>
+        <translation>Número de teléfono</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockAddSource</name>
+    <message>
+        <source>Add Block Source</source>
+        <translation>Añadir fuente de bloqueo</translation>
+    </message>
+    <message>
+        <source>No sources to add</source>
+        <translation>No hay fuentes para añadir</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockContact</name>
+    <message>
+        <source>Block Contact</source>
+        <translation>Bloquear contacto</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockHistory</name>
+    <message>
+        <source>History</source>
+        <translation>Historial</translation>
+    </message>
+    <message>
+        <source>Lookup</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>No events</source>
+        <translation>No hay resultados</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockStart</name>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Call Blocks</source>
+        <translation>Bloqueo de llamadas</translation>
+    </message>
+    <message>
+        <source>No blocks activated</source>
+        <translation>No hay bloqueos activados</translation>
+    </message>
+    <message>
+        <source>Add Block Source</source>
+        <translation>Añadir fuente de bloqueo</translation>
+    </message>
+    <message>
+        <source>Block Contact</source>
+        <translation>Bloquear contacto</translation>
+    </message>
+    <message>
+        <source>Block Number</source>
+        <translation>Bloquear número</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>Deleting %1</source>
+        <translation>Borrando %1</translation>
+    </message>
+</context>
+<context>
+    <name>PageBotDetails</name>
+    <message>
+        <source>Clear Cache</source>
+        <translation>Limpiar caché</translation>
+    </message>
+    <message>
+        <source>Report a Problem</source>
+        <translation>Informar de un problema</translation>
+    </message>
+    <message>
+        <source>Test Number</source>
+        <translation>Probar número</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <source>Bot Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Ninguno</translation>
+    </message>
+    <message>
+        <source>Enable Auto-blocking</source>
+        <translation>Habilitar auto-bloqueo</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Iniciar sesión</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Descripción</translation>
+    </message>
+    <message>
+        <source>Link</source>
+        <translation>Enlace</translation>
+    </message>
+</context>
+<context>
+    <name>PageBotDownload</name>
+    <message>
+        <source>Install</source>
+        <translation>Instalar</translation>
+    </message>
+    <message>
+        <source>Reverse caller lookup</source>
+        <translation>Búsqueda inversa de llamadas</translation>
+    </message>
+    <message>
+        <source>Search for people</source>
+        <translation>Búsqueda de particulares</translation>
+    </message>
+    <message>
+        <source>Search for businesses</source>
+        <translation>Búsqueda de empresas</translation>
+    </message>
+    <message>
+        <source>Auto-blocking</source>
+        <translation>Auto-bloqueo</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Estado</translation>
+    </message>
+    <message>
+        <source>Not installed yet</source>
+        <translation>No instalada aún</translation>
+    </message>
+    <message>
+        <source>Installed and up-to-date</source>
+        <translation>Instalada y actualizada</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Reiniciar</translation>
+    </message>
+    <message>
+        <source>Update available</source>
+        <translation>Actualización disponible</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>A newer version of phonehook is required</source>
+        <translation>Es necesario instalar una versión más reciente de Phonehook</translation>
+    </message>
+    <message>
+        <source>Link</source>
+        <translation>Enlace</translation>
+    </message>
+    <message>
+        <source>Capabilities</source>
+        <translation>Funciones</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Descripción</translation>
+    </message>
+</context>
+<context>
+    <name>PageBotTest</name>
+    <message>
+        <source>Phone Number</source>
+        <translation>Número de teléfono</translation>
+    </message>
+    <message>
+        <source>Run</source>
+        <translation>Ejecutar</translation>
+    </message>
+</context>
+<context>
+    <name>PageCallLogStart</name>
+    <message>
+        <source>Missed Calls</source>
+        <translation>Llamadas perdidas</translation>
+    </message>
+    <message>
+        <source>Lookup</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>Block</source>
+        <translation>Bloquear</translation>
+    </message>
+    <message>
+        <source>Incoming Calls</source>
+        <translation>Llamadas entrantes</translation>
+    </message>
+    <message>
+        <source>Outgoing Calls</source>
+        <translation>Llamadas salientes</translation>
+    </message>
+    <message>
+        <source>Missed</source>
+        <translation>Perdidas</translation>
+    </message>
+    <message>
+        <source>Incoming</source>
+        <translation>Entrantes</translation>
+    </message>
+    <message>
+        <source>Outgoing</source>
+        <translation>Salientes</translation>
+    </message>
+    <message>
+        <source>Calls</source>
+        <translation>Llamadas</translation>
+    </message>
+</context>
+<context>
+    <name>PageDownloadWait</name>
+    <message>
+        <source>Download Error :(</source>
+        <translation>Errore de descarga :(</translation>
+    </message>
+</context>
+<context>
+    <name>PageSearchResults</name>
+    <message>
+        <source>Results</source>
+        <translation>Resultados</translation>
+    </message>
+    <message>
+        <source>No results</source>
+        <translation>No hay resultados</translation>
+    </message>
+</context>
+<context>
+    <name>PageSearchStart</name>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>What?</source>
+        <translation>¿Qué?</translation>
+    </message>
+    <message>
+        <source>Where?</source>
+        <translation>¿Dónde?</translation>
+    </message>
+    <message>
+        <source>Search for</source>
+        <translation>Búsqueda de</translation>
+    </message>
+    <message>
+        <source>People</source>
+        <translation>Particulares</translation>
+    </message>
+    <message>
+        <source>Businesses</source>
+        <translation>Empresas</translation>
+    </message>
+    <message>
+        <source>Search at</source>
+        <extracomment>referring to what source to use</extracomment>
+        <translation>Búsqueda desde</translation>
+    </message>
+    <message>
+        <source>There are no installed sources with search capability</source>
+        <translation>No hay instaladas fuentes para la función de búsqueda</translation>
+    </message>
+</context>
+<context>
+    <name>PageServerBotList</name>
+    <message>
+        <source>Available Sources</source>
+        <translation>Fuentes disponibles</translation>
+    </message>
+    <message>
+        <source>Failed to load</source>
+        <translation>Falló al cargar</translation>
+    </message>
+    <message>
+        <source>All names and logos listed here are properties of respective rights holders. Phonehook is not endorsed by any of these services. </source>
+        <translation>Todos los nombres y logotipos mencionados aquí son propiedad de los titulares de los derechos respectivos. Phonehook no está respaldada por ninguno de estos servicios. </translation>
+    </message>
+</context>
+</TS>
