6754f29cdf4093bb3d6362e2dcba878e5e292056
Ancelad <i_am_pomidorchik@mail.ru>
2016-08-17 15:28:44 +0500

    Russian translation for daemon

---
+++ client/daemon/translations/phonehook-daemon-ru.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>gui</name>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="38"/>
+        <source>Name</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="39"/>
+        <source>Address</source>
+        <translation>Адрес</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="40"/>
+        <source>Country</source>
+        <translation>Страна</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="42"/>
+        <source>Operator</source>
+        <extracomment>network operator</extracomment>
+        <translation>Оператор</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="44"/>
+        <source>Properties</source>
+        <extracomment>owner properties</extracomment>
+        <translation>Недвижимость</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="46"/>
+        <source>SSN</source>
+        <extracomment>social security number/ personal number</extracomment>
+        <translation>Номер социального страхования</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="47"/>
+        <source>Marital Status</source>
+        <translation>Семейное положение</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="48"/>
+        <source>Vehicles</source>
+        <translation>Транспорт</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="49"/>
+        <source>Organization Number</source>
+        <translation>Номер организации</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="50"/>
+        <source>Company Information</source>
+        <translation>Информация о компании</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="51"/>
+        <source>Remarks</source>
+        <translation>Примечания</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="52"/>
+        <source>Owner</source>
+        <translation>Владелец</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="53"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="54"/>
+        <source>Email</source>
+        <translation>Эл. почта</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="55"/>
+        <source>Website</source>
+        <translation>Сайт</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="56"/>
+        <source>Profession</source>
+        <translation>Вид деятельности</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="57"/>
+        <source>Category</source>
+        <translation>Категория</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="58"/>
+        <source>Comment</source>
+        <translation>Комментарий</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="59"/>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="60"/>
+        <source>Age</source>
+        <translation>Возраст</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="205"/>
+        <source>No results</source>
+        <translation>Нет результатов</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="411"/>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="432"/>
+        <source>Block</source>
+        <translation>Блок</translation>
+    </message>
+    <message>
+        <location filename="../qml/popup/gui.qml" line="476"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+</context>
+</TS>
