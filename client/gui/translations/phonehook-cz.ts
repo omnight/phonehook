b67c4810ae840a2e5e62b943c516eaa682d7ef2a
Armin Felder <armin.felder@osalliance.com>
2019-07-31 21:36:30 +0200

    adds czech translation by Malakay

---
+++ client/gui/translations/phonehook-cz.ts
@@ -0,0 +1,448 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ" sourcelanguage="cs">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Phonehook</source>
+        <translation type="unfinished">Phonehook</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Phonehook</source>
+        <translation type="unfinished">Phonehook</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished">Nastavení</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="unfinished">Odstranit</translation>
+    </message>
+    <message>
+        <source>Data Sources</source>
+        <translation type="unfinished">Zdroje dat</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished">Hledat</translation>
+    </message>
+    <message>
+        <source>Blocks</source>
+        <translation type="unfinished">Blokování</translation>
+    </message>
+    <message>
+        <source>Call Log</source>
+        <translation type="unfinished">Historie</translation>
+    </message>
+    <message>
+        <source>Daemon</source>
+        <translation type="unfinished">Služba</translation>
+    </message>
+    <message>
+        <source>Running</source>
+        <translation type="unfinished">Běží</translation>
+    </message>
+    <message>
+        <source>Not Running</source>
+        <translation type="unfinished">Neběží</translation>
+    </message>
+    <message>
+        <source>Start Daemon</source>
+        <translation type="unfinished">Spustit službu</translation>
+    </message>
+    <message>
+        <source>Installed Sources</source>
+        <translation type="unfinished">Instalované zdroje</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <extracomment>No installed sources</extracomment>
+        <translation type="unfinished">Žádné</translation>
+    </message>
+</context>
+<context>
+    <name>PageAppSettings</name>
+    <message>
+        <source>App Settings</source>
+        <translation type="unfinished">Nastavení aplikace</translation>
+    </message>
+    <message>
+        <source>Basic</source>
+        <translation type="unfinished">Základní</translation>
+    </message>
+    <message>
+        <source>Show only for unknown contacts</source>
+        <translation type="unfinished">Zobrazovat pouze pro neznámé kontakty</translation>
+    </message>
+    <message>
+        <source>Old popup design</source>
+        <translation type="unfinished">Starý design</translation>
+    </message>
+    <message>
+        <source>Enable while roaming</source>
+        <translation type="unfinished">Povolit při roamingu</translation>
+    </message>
+    <message>
+        <source>Popup Timeout</source>
+        <translation type="unfinished">Timeout pro popup</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation type="unfinished">Zakázáno</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation type="unfinished">Pokročilé</translation>
+    </message>
+    <message>
+        <source>Kill dialer when blocking call</source>
+        <translation type="unfinished">Při blokování hovoru skrýt klávesnici</translation>
+    </message>
+    <message>
+        <source>Restart Daemon</source>
+        <translation type="unfinished">Restartovat službu</translation>
+    </message>
+    <message>
+        <source>Auto Update Sources</source>
+        <translation type="unfinished">Automaticky aktualizovat zdroje</translation>
+    </message>
+    <message>
+        <source>Source Test Mode</source>
+        <translation type="unfinished">Testovací mód zdroje</translation>
+    </message>
+    <message>
+        <source> s</source>
+        <translation type="unfinished"> s</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockAddManual</name>
+    <message>
+        <source>Add Block</source>
+        <translation type="unfinished">Přidat</translation>
+    </message>
+    <message>
+        <source>Add a number to be blocked. Number may be either local or international variety. Use * for wildcard matching.</source>
+        <translation type="unfinished">Přidejte číslo pro zablokování. Může být v lokálním i mezinárodním tvaru. Pro wildcard použijte * .</translation>
+    </message>
+    <message>
+        <source>Hidden number</source>
+        <extracomment>toggle switch for hidden number</extracomment>
+        <translation type="unfinished">Skryté číslo</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <extracomment>placeholder for name input</extracomment>
+        <translation type="unfinished">Název</translation>
+    </message>
+    <message>
+        <source>Phone Number</source>
+        <extracomment>placeholder for number input</extracomment>
+        <translation type="unfinished">Telefonní číslo</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockAddSource</name>
+    <message>
+        <source>Add Block Source</source>
+        <translation type="unfinished">Přidat blokování ze zdroje</translation>
+    </message>
+    <message>
+        <source>No sources to add</source>
+        <translation type="unfinished">Žádné zdroje k přidání</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockContact</name>
+    <message>
+        <source>Block Contact</source>
+        <translation type="unfinished">Blokovat kontakt</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockHistory</name>
+    <message>
+        <source>History</source>
+        <translation type="unfinished">Historie</translation>
+    </message>
+    <message>
+        <source>Lookup</source>
+        <translation type="unfinished">Vyhledat</translation>
+    </message>
+    <message>
+        <source>No events</source>
+        <translation type="unfinished">Žádné události</translation>
+    </message>
+</context>
+<context>
+    <name>PageBlockStart</name>
+    <message>
+        <source>Delete</source>
+        <translation type="unfinished">Odstranit</translation>
+    </message>
+    <message>
+        <source>Call Blocks</source>
+        <translation type="unfinished">Blokované</translation>
+    </message>
+    <message>
+        <source>No blocks activated</source>
+        <translation type="unfinished">Nejsou aktivní žádná blokování</translation>
+    </message>
+    <message>
+        <source>Add Block Source</source>
+        <translation type="unfinished">Přidat zdroj k blokování</translation>
+    </message>
+    <message>
+        <source>Block Contact</source>
+        <translation type="unfinished">Blokovat kontakt</translation>
+    </message>
+    <message>
+        <source>Block Number</source>
+        <translation type="unfinished">Blokovat číslo</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished">Obnovit</translation>
+    </message>
+    <message>
+        <source>Deleting %1</source>
+        <translation type="unfinished">Mazání %1</translation>
+    </message>
+</context>
+<context>
+    <name>PageBotDetails</name>
+    <message>
+        <source>Clear Cache</source>
+        <translation type="unfinished">Vyčistit cache</translation>
+    </message>
+    <message>
+        <source>Report a Problem</source>
+        <translation type="unfinished">Nahlásit problém</translation>
+    </message>
+    <message>
+        <source>Test Number</source>
+        <translation type="unfinished">Testovat číslo</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished">Odstranit</translation>
+    </message>
+    <message>
+        <source>Bot Settings</source>
+        <translation type="unfinished">Nastavení</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="unfinished">Žádné</translation>
+    </message>
+    <message>
+        <source>Enable Auto-blocking</source>
+        <translation type="unfinished">Povolit automatické blokování</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation type="unfinished">Přihlášení</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation type="unfinished">Popis</translation>
+    </message>
+    <message>
+        <source>Link</source>
+        <translation type="unfinished">Odkaz</translation>
+    </message>
+</context>
+<context>
+    <name>PageBotDownload</name>
+    <message>
+        <source>Reverse caller lookup</source>
+        <translation type="unfinished">Vyhledání volajícího</translation>
+    </message>
+    <message>
+        <source>Search for people</source>
+        <translation type="unfinished">Hledat lidi</translation>
+    </message>
+    <message>
+        <source>Search for businesses</source>
+        <translation type="unfinished">Hledat společnosti</translation>
+    </message>
+    <message>
+        <source>Auto-blocking</source>
+        <translation type="unfinished">Auto-blokování</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation type="unfinished">Stav</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation type="unfinished">Reset</translation>
+    </message>
+    <message>
+        <source>Update available</source>
+        <translation type="unfinished">Dostupný update</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished">Aktualizovat</translation>
+    </message>
+    <message>
+        <source>A newer version of phonehook is required</source>
+        <translation type="unfinished">Je třeba nainstalovat novější verzi Phonehook</translation>
+    </message>
+    <message>
+        <source>Link</source>
+        <translation type="unfinished">Odkaz</translation>
+    </message>
+    <message>
+        <source>Capabilities</source>
+        <translation type="unfinished">Služby</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation type="unfinished">Popis</translation>
+    </message>
+    <message>
+        <source>Activate</source>
+        <translation type="unfinished">Aktivovat</translation>
+    </message>
+    <message>
+        <source>Not activated</source>
+        <translation type="unfinished">Neaktivní</translation>
+    </message>
+    <message>
+        <source>Active and up-to-date</source>
+        <translation type="unfinished">Aktivní a aktuální</translation>
+    </message>
+</context>
+<context>
+    <name>PageBotTest</name>
+    <message>
+        <source>Phone Number</source>
+        <translation type="unfinished">Telefonní číslo</translation>
+    </message>
+    <message>
+        <source>Run</source>
+        <translation type="unfinished">Spustit</translation>
+    </message>
+</context>
+<context>
+    <name>PageCallLogStart</name>
+    <message>
+        <source>Missed Calls</source>
+        <translation type="unfinished">Zmeškané</translation>
+    </message>
+    <message>
+        <source>Lookup</source>
+        <translation type="unfinished">Vyhledat</translation>
+    </message>
+    <message>
+        <source>Block</source>
+        <translation type="unfinished">Blokovat</translation>
+    </message>
+    <message>
+        <source>Incoming Calls</source>
+        <translation type="unfinished">Příchozí</translation>
+    </message>
+    <message>
+        <source>Outgoing Calls</source>
+        <translation type="unfinished">Odchozí</translation>
+    </message>
+    <message>
+        <source>Missed</source>
+        <translation type="unfinished">Zmeškané</translation>
+    </message>
+    <message>
+        <source>Incoming</source>
+        <translation type="unfinished">Příchozí</translation>
+    </message>
+    <message>
+        <source>Outgoing</source>
+        <translation type="unfinished">Odchozí</translation>
+    </message>
+    <message>
+        <source>Calls</source>
+        <translation type="unfinished">Hovory</translation>
+    </message>
+</context>
+<context>
+    <name>PageDownloadWait</name>
+    <message>
+        <source>Download Error :(</source>
+        <translation type="unfinished">Stahování selhalo :(</translation>
+    </message>
+</context>
+<context>
+    <name>PageOAuth</name>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished">Zrušit</translation>
+    </message>
+</context>
+<context>
+    <name>PageSearchResults</name>
+    <message>
+        <source>Results</source>
+        <translation type="unfinished">Výsledky</translation>
+    </message>
+    <message>
+        <source>No results</source>
+        <translation type="unfinished">Žádné výsledky</translation>
+    </message>
+</context>
+<context>
+    <name>PageSearchStart</name>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished">Hledat</translation>
+    </message>
+    <message>
+        <source>What?</source>
+        <translation type="unfinished">Co hledat?</translation>
+    </message>
+    <message>
+        <source>Where?</source>
+        <translation type="unfinished">Kde?</translation>
+    </message>
+    <message>
+        <source>Search for</source>
+        <translation type="unfinished">Hledat</translation>
+    </message>
+    <message>
+        <source>People</source>
+        <translation type="unfinished">Osoby</translation>
+    </message>
+    <message>
+        <source>Businesses</source>
+        <translation type="unfinished">Firmy</translation>
+    </message>
+    <message>
+        <source>Search at</source>
+        <extracomment>referring to what source to use</extracomment>
+        <translation type="unfinished">Hledat v</translation>
+    </message>
+    <message>
+        <source>There are no installed sources with search capability</source>
+        <translation type="unfinished">Nejsou nainstalovány žádné zdroje s funkcí hledání</translation>
+    </message>
+</context>
+<context>
+    <name>PageServerBotList</name>
+    <message>
+        <source>Available Sources</source>
+        <translation type="unfinished">Dostupné zdroje</translation>
+    </message>
+    <message>
+        <source>Failed to load</source>
+        <translation type="unfinished">Nelze načíst</translation>
+    </message>
+    <message>
+        <source>All names and logos listed here are properties of respective rights holders. Phonehook is not endorsed by any of these services. </source>
+        <translation type="unfinished">Všechny názvy a loga zde zobrazené jsou vlastnictvím držitelů práv. Phonehook není spojen s žádnou z těchto služeb. </translation>
+    </message>
+</context>
+</TS>
